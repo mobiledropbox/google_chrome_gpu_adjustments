ac74d6dc0c06ea79c53465274dfdd3af1c911c42
aeronje <aeronje@hotmail.com>
2025-11-11 22:02:18 +0800

    Updated target URL and author date in script including SHA immutable codes kasi pagod na ako mag-antay kay mobiledropbox magpurge ng magpurge sa jsdelivr lalamove na iyan.
    Updated target URL and author date in script including SHA immutable codes kasi pagod na ako mag-antay kay mobiledropbox magpurge ng magpurge sa jsdelivr lalamove na iyan.

--- google_chrome_gpu_adjustments.js
+++ google_chrome_gpu_adjustments.js
@@ -1,6 +1,6 @@
-// Ron Penones | November 10th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
+// Ron Penones | November 11th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
 
-export const targetURL = "https://cdn.jsdelivr.net/gh/mobiledropbox/landing_pages_scripts@main/google_chrome_gpu_adjustments/google_chrome_gpu.html";
+export const targetURL = "https://cdn.jsdelivr.net/gh/mobiledropbox/landing_pages_scripts@7e510ef4041e4a0b27e2f264182aea49767a0edb/google_chrome_gpu_adjustments/google_chrome_gpu.html";
 
 export async function loadAndWriteHTML() {
   try {
